702fcac5fa83a4bd0e490d4d8396cb6afdb70f11
ecorpus <eanocorpus@gmail.com>
2019-01-21 17:58:36 -0500

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -12,3 +12,4 @@ class profile::ssh_server {
 		type   => 'ssh-rsa',
 		key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQCspPqVjkxkQhmCfW8ZZQfPvSC2powQDTl4Bi5F+WVrfZChpPIK1TS++QHK1oJhNVPGsSEr1Mq9egQ+8wEfbebiIWd5jFdqAXgDsF1WbuzCGGyB+kJY/HiVV1d2+/9LZoVl1i/1UeUWH3NI4C2Cf30QL5t92SoMdZEhlWzEzs/apvX8BsyWHeesozKKtjJVgKEE1nDrPvdnillwoVEzt9xCd/+nFUdBJWf8PDjr2U8WSeOWAS6JCYflXPEMzMzgcCi2fNmMKr6qauYMSjGR98Uvjzl8AYhJkEvCPz72GzvepOx0WAXUUsZhgUQhPWLaa6AULHpLRyqXqoZvf/u/Ou03',
 	}
+}
